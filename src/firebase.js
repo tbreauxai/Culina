93270fdd81a9afd3120414143aaea9e8cb0f3d15
tbreauxai <tbreauxai@gmail.com>
2026-04-08 23:32:14 +0000

    firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDYb-UhORqDvHpmavFt3fSf2OMbhSdo8Qs",
+  authDomain: "culina-f1030.firebaseapp.com",
+  projectId: "culina-f1030",
+  storageBucket: "culina-f1030.firebasestorage.app",
+  messagingSenderId: "892727431945",
+  appId: "1:892727431945:web:42f5cc3e6f0034c53ab9d8"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
